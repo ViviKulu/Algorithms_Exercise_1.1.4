9b53dd06b1332c9c1d16d7f49a8bcc3d3b8e16fa
Vivian Babirye <babimaji1111@gmail.com>
2018-12-19 22:52:08 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    //    What (if anything) is wrong with each of the following statements?
+
+    //    a. if (a > b) then c = 0;
+    //    b. if a > b { c = 0; }
+    //    c. if (a > b) c = 0;
+    //    d. if (a > b) c = 0 else b = 0;
+
+    //    a. if (a > b) then c = 0; (Then should not be apart of the statement)
+    //    b. if a > b { c = 0; } (This statement is missing the parenthesis around a > b)
+    //    c. if (a > b) c = 0; (Nothing is wrong with this statement)
+    //    d. if (a > b) c = 0 else b = 0; (this is missing the curly braces after (a > b))
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
